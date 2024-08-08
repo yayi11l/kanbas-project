76feb948fa0ca1ff1d9e446f359dcf220730189a
赵晨曦 <zhao.chenxi1@northeastern.edu>
2024-08-08 16:26:19 -0400

    poAct

--- social-web-react-project/src/content/Home/Feed/index.tsx
+++ social-web-react-project/src/content/Home/Feed/index.tsx
@@ -1,13 +1,13 @@
 import Posts from "./Posts/Posts";
 import InputBox from "./InputBox";
-import Stories from "./Stories";
+// import Stories from "./Stories";
 
 export default function Feed() {
   return (
     <div className="flex-grow h-screen pb-44 pt-6 mr-4
     xl:mr-40 overflow-y-auto scrollbar-hide">
       <div className="mx-auto md:max-w-lg lg: max-w-2xl">
-        <Stories />
+        {/* <Stories /> */}
         {/* <InputBox /> */}
         <Posts />
       </div>
---
+++ social-web-react-project/src/content/Profile/Activity/postActs/index.tsx
@@ -0,0 +1,6 @@
+export default function postActs() {
+    return (
+        <h1> test</h1>
+
+    );
+}
